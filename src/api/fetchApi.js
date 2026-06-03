7cda6606bea256496ba8904cb676e58c5aed98ef
Nagendra-in-hackingly <nagendra@hackingly.in>
2026-06-03 10:06:54 +0530

    url added

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,5 +1,8 @@
+// src/api/fetchApi.js
+const BASE_URL = import.meta.env.VITE_API_URL || '';
+
 export async function fetchApi(url) {
-  const res = await fetch(url);
+  const res = await fetch(`https://ev-fleet-backend-9yab.onrender.com${url}`);
   if (!res.ok) throw new Error(`HTTP ${res.status}`);
   return res.json();
 }
